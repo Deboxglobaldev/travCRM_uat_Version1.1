d54a6fdf0a3649480c66463395c813b817f9d941
Neelakshibaghe <neelakshi.baghel@deboxglobal.com>
2024-01-29 17:05:48 +0530

    Login Authentication

---
+++ Code/Services/Others/app/Http/Controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class AuthController extends Controller
+{
+    public function loadLogin(){
+        return view('login');
+    }
+}
